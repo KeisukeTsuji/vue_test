a26c5af630a2a284cdae81425b79261999039fcc
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-04-17 17:44:57 +0900

    exaple.spec.tsにテストコード作成

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,11 +2,27 @@ import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+  it("カウンターの初期表示が0", () => {
+    const wrapper = shallowMount(HelloWorld);
+    expect(wrapper.find(".counter").text()).toMatch("0");
+  });
+
+  it("ボタンクリックで1増加する", async () => {
+    const wrapper = shallowMount(HelloWorld);
+    expect(wrapper.find(".counter").text()).toMatch("0");
+
+    wrapper.find("button").trigger("click");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.find(".counter").text()).toMatch("1");
+  });
+
+  it("ボタンクリックでincrement1回イベントが発生する", () => {
+    const wrapper = shallowMount(HelloWorld);
+
+    wrapper.find("button").trigger("click");
+
+    expect(wrapper.emitted("onIncrement")).toBeTruthy();
+    expect(wrapper.emitted("onIncrement")?.length).toBe(1);
   });
 });
